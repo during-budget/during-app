e7827083b924a69989c39a77f6e731f7b810e7b3
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-02-21 15:38:04 +0900

    Feat: Payment / Implement InApp Payment using react-native-iap (android) #4

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,10 +1,31 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {BackHandler, Dimensions, Platform, StyleSheet} from 'react-native';
+import {
+  Alert,
+  BackHandler,
+  Dimensions,
+  Platform,
+  StyleSheet,
+} from 'react-native';
 import {
   BannerAdSize,
   GAMBannerAd,
   TestIds,
 } from 'react-native-google-mobile-ads';
+import {
+  ProductPurchase,
+  PurchaseError,
+  Sku,
+  SubscriptionPurchase,
+  clearTransactionIOS,
+  endConnection,
+  finishTransaction,
+  flushFailedPurchasesCachedAsPendingAndroid,
+  getProducts,
+  initConnection,
+  purchaseErrorListener,
+  purchaseUpdatedListener,
+  requestPurchase,
+} from 'react-native-iap';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
 import {WebViewNativeEvent} from 'react-native-webview/lib/RNCWebViewNativeComponent';
@@ -13,6 +34,9 @@ const Main = () => {
   const webview = useRef<WebView>(null);
   const [navState, setNaveState] = useState<WebViewNativeEvent>();
 
+  const [purchaseUpdate, setPurchaseUpdate] = useState<any>(null);
+  const [purchaseError, setPurchaseError] = useState<any>(null);
+
   const unitID =
     Platform.select({
       ios: 'ca-app-pub-7896727622535419/3140414754',
@@ -20,6 +44,80 @@ const Main = () => {
     }) || '';
 
   const adUnitId = __DEV__ ? TestIds.BANNER : unitID;
+
+  useEffect(() => {
+    const setPayment = async () => {
+      try {
+        await initConnection();
+        if (Platform.OS === 'android') {
+          await flushFailedPurchasesCachedAsPendingAndroid();
+        } else {
+          await clearTransactionIOS();
+        }
+      } catch (error) {
+        if (error instanceof PurchaseError) {
+          console.error(
+            'An error happened',
+            `[${error.code}]: ${error.message}`,
+            error,
+          );
+        } else {
+          console.error('An error happend', 'finishTransaction', error);
+        }
+      }
+
+      setPurchaseUpdate(
+        purchaseUpdatedListener(
+          async (purchase: ProductPurchase | SubscriptionPurchase) => {
+            const receipt = purchase.transactionReceipt
+              ? purchase.transactionReceipt
+              : (purchase as unknown as {originalJson: string}).originalJson;
+            if (receipt) {
+              try {
+                await finishTransaction({purchase});
+
+                webview.current?.postMessage(
+                  JSON.stringify({
+                    intent: 'payment',
+                    content: purchase.productId,
+                  }),
+                );
+              } catch (error) {
+                webview.current?.postMessage(
+                  JSON.stringify({
+                    intent: 'purchase_error',
+                  }),
+                );
+              }
+            }
+          },
+        ),
+      );
+
+      setPurchaseError(
+        purchaseErrorListener((error: PurchaseError) => {
+          webview.current?.postMessage(
+            JSON.stringify({
+              intent: 'purchase_error',
+            }),
+          );
+          console.error(
+            'An error happened',
+            `[${error.code}]: ${error.message}`,
+            error,
+          );
+        }),
+      );
+    };
+    setPayment();
+
+    return () => {
+      purchaseUpdate?.remove();
+      purchaseError?.remove();
+
+      endConnection();
+    };
+  }, []);
 
   useEffect(() => {
     const canGoBack = navState?.canGoBack;
@@ -40,21 +138,63 @@ const Main = () => {
     };
   }, [navState?.canGoBack]);
 
+  const handleOnMessage = async (e: any) => {
+    const msg = e.nativeEvent.data;
+    const obj = JSON.parse(msg);
+    switch (obj.intent) {
+      case 'payment':
+        const id = obj.content;
+        const products = await getProducts({skus: [id]});
+
+        try {
+          if (products[0]) {
+            await requestPurchase({sku: products[0].productId});
+          } else {
+            throw 'id not exists on product list';
+          }
+        } catch (error) {
+          if (error instanceof PurchaseError) {
+            console.error(
+              'An error happened',
+              `[${error.code}]: ${error.message}`,
+              error,
+            );
+          } else {
+            console.error('An error happend', 'finishTransaction', error);
+          }
+        }
+
+        // const paymentCompleted = true;
+        // if (paymentCompleted) {
+        //   webview.current?.postMessage(
+        //     JSON.stringify({intent: 'payment', content: id}),
+        //   );
+        // }
+        break;
+      case 'test':
+        Alert.alert(obj.content);
+      default:
+        return;
+    }
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['top', 'left', 'right']} style={styles.container}>
         <WebView
           ref={webview}
-          source={{uri: 'https://during.money'}}
+          source={{uri: 'https://www.during.money'}}
           style={styles.webview}
           onNavigationStateChange={event => setNaveState(event)}
           onLoadEnd={() => {
             webview.current?.postMessage(
               JSON.stringify({
-                platform: Platform.OS,
+                intent: 'platform',
+                content: Platform.OS,
               }),
             );
           }}
+          onMessage={handleOnMessage}
         />
         <GAMBannerAd
           unitId={adUnitId}
